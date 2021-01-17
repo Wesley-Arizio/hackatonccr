6176254c650d91221a019e88d1836e57c2748942
wesley arizio <45431385+Wesley-Arizio@users.noreply.github.com>
2021-01-17 00:43:12 -0300

    adicionando authController e middleware para o app

--- back_end/src/app/app.js
+++ back_end/src/app/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
 const UserController = require("../controller/userController/userController");
+const AuthController = require("../controller/authController/authController");
+const AuthMiddleware = require("../controller/middleware/auth");
 
 const app = express();
 
 app.use(express.json());
 app.use(UserController);
+app.use(AuthController);
+app.use(AuthMiddleware);
 
 module.exports = app;
